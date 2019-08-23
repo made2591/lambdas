586b4b339827dcd8219a0367819a0db337f06f44
Matteo Madeddu <matteo.madeddu@gmail.com>
2019-08-23 13:59:26 +0200

    Fix credentials

--- init.sh
+++ init.sh
@@ -6,5 +6,6 @@ cat > ~/.aws/credentials << EOL
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-aws_region = ${AWS_REGION}
+region = ${AWS_REGION}
+output = json
 EOL
